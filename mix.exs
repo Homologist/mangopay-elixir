565e730fa87e4c39237a089a76d64b31dcfab6db
SocFoot <nzakou.cedric@gmail.com>
2018-03-17 11:50:40 +0100

    update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Mangopay.Mixfile do
     [
       app: :mangopay,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       start_permanent: Mix.env == :prod,
       deps: deps(),
       preferred_cli_env: [
