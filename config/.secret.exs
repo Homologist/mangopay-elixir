3193eda5333a4ce68b683bcf9cf2119ba9bbd3a1
Homologist <nzakou.cedric@protonmail.com>
2018-04-20 13:52:37 +0200

    untrack config

--- config/.secret.exs
+++
@@ -1,4 +0,0 @@
-use Mix.Config
-config :mangopay,
-  client_id: "sdk-unit-tests",
-  passphrase: "cqFfFrWfCcb7UadHNxx2C9Lo6Djw8ZduLi7J9USTmu8bhxxpju"
--- config/config.exs
+++
@@ -1,3 +0,0 @@
-use Mix.Config
-import_config ".secret.exs"
-IO.puts "import done"
