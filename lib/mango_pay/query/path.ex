022e8b0a46382ab3be889ebe48ffa7b2a7a36c3a
Homologist <nzakou.cedric@protonmail.com>
2018-04-22 20:50:09 +0200

    add comment for query path

--- lib/mango_pay/query/path.ex
+++ lib/mango_pay/query/path.ex
@@ -1,4 +1,5 @@
 defmodule MangoPay.Query.Path do
+  @moduledoc false
   defmacro set_path(path) do
     quote do
       defp resource do
@@ -9,6 +10,13 @@ defmodule MangoPay.Query.Path do
         "#{resource()}/#{id}"
       end
 
+      @doc """
+      Get path called.
+    
+      ## Examples
+          "/users/id" = MangoPay.User.path(id)
+    
+      """
       def path(), do: resource()
       def path(id), do: resource(id)
     end
